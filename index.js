51f5d97fe145d53091114e2dd36ff3622201cb17
kawsar-uddin <kawsaruddin450@gmail.com>
2024-08-27 10:23:14 +0600

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res)=> {
+    res.send(`Brainyplay server is running at: ${port}`)
+})
+
+app.listen(port, ()=> {
+    console.log(`Server is running at port:`, port);
+})
